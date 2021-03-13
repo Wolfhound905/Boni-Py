f7013b47777df8e7c1c5ed02d8511a3b9394bc67
Wolfhound905 <aiden8green@gmail.com>
2021-03-12 22:45:44 -0500

    oops forgot one

--- behaviors/help.py
+++ behaviors/help.py
@@ -11,7 +11,7 @@ class helpMenu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
      
-    @cog_ext.cog_slash(name="help", description='Use this command and I will give you the answers to life.', guild_ids = [GUILD_ID])
+    @cog_ext.cog_slash(name="help", description='Use this command and I will give you the answers to life.', guild_ids = [guilds])
     async def group_say(self, ctx: SlashContext):
         embed=discord.Embed(title="Help Menu", description="A list of all slash commands to use with Boni", color=0xf6c518)
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/819606905735479356/f30474b0aa66b3541bfdacf5bad5783a.png")
