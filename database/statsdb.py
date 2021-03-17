db4779943e4d0813a1c7117a8e28f087ee4adade
Wolfhound905 <aiden8green@gmail.com>
2021-03-17 19:55:05 -0400

    fixed database timeout connection

--- database/statsdb.py
+++ database/statsdb.py
@@ -1,7 +1,6 @@
 import os
 import discord
-from configuration import (get_user_name, get_password,
-                           get_user_name, get_host, get_admins, get_database)
+from configuration import (get_user_name, get_password, get_user_name, get_host, get_admins, get_database)
 import mysql.connector
 
 db = mysql.connector.connect(user=get_user_name(), password=get_password(), host=get_host(), database=get_database())
@@ -30,6 +29,8 @@ class Stats():
 
 
 def get_stats() -> Stats:
+    db = mysql.connector.connect(user=get_user_name(), password=get_password(), host=get_host(), database=get_database())
+    sql = db.cursor()
     # Example: "season" is row[0]
     sql.execute(
         "SELECT season, wins, losses, win_streak, loss_streak, max_win_streak, max_loss_streak from stats WHERE season = (SELECT Max(season) FROM stats)")
@@ -37,7 +38,6 @@ def get_stats() -> Stats:
 
     for row in rows:
         return Stats(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
-
     sql.close()
 
     # If no rows exist then raise an error
@@ -45,6 +45,8 @@ def get_stats() -> Stats:
 
 
 def increment_win():
+    db = mysql.connector.connect(user=get_user_name(), password=get_password(), host=get_host(), database=get_database())
+    sql = db.cursor()
     stats = get_stats()
     season = str(stats.season)
     new_max_win_streak = str(max(stats.win_streak + 1, stats.max_win_streak))
@@ -56,7 +58,9 @@ def increment_win():
     sql.close()
 
 
-def increment_loss():
+def increment_loss():    
+    db = mysql.connector.connect(user=get_user_name(), password=get_password(), host=get_host(), database=get_database())
+    sql = db.cursor()
     stats = get_stats()
     season = str(stats.season)
     new_max_loss_streak = str(max(stats.loss_streak + 1, stats.max_loss_streak))
@@ -68,6 +72,8 @@ def increment_loss():
     sql.close()
 
 def increment_new_season():
+    db = mysql.connector.connect(user=get_user_name(), password=get_password(), host=get_host(), database=get_database())
+    sql = db.cursor()
     stats = get_stats()
     new_season = stats.season + 1
     sql.execute(f"""
