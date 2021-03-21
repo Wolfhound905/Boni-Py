d9e63230e1be0dbfb812ebd455e7bf58aaf785c8
Wolfhound905 <aiden8green@gmail.com>
2021-03-20 23:25:39 -0400

    tier based streak messages

--- behaviors/clubMatches.py
+++ behaviors/clubMatches.py
@@ -38,35 +38,59 @@ class clubMatches(commands.Cog):
 
     @cog_ext.cog_slash(name="club", options=options, description='Reports wins and losses or even view our stats!', guild_ids=guilds)
     async def group_say(self, ctx: SlashContext, match: str):
-        win_messages = [
-            "Way to rep the club!", "Wow! You guys are on a roll!", "Now that’s how you slam!",
-            "I wanna grow up to slam as hard as you guys one day!", "They won’t forget the day they lost to the Slambonis!",
-            "Do you think we should go pro?", "RLCS here we come!"
-        ]
-        loss_messages = [
-            "Aw man…", "I still think we’re the best club around!", "At least we had fun",
-            "Papa Boni always used to say “Sometimes you slam, sometimes you get slammed”",
-            "Darn. I forgot what I was gonna to say…", "Well we’ve still got each other"
-        ]
+        Messages = {
+            "W1": ["Way to rep the club!",
+                   "Wow! You guys are on a roll!",
+                   "Now that’s how you slam!",
+                   "I wanna grow up to slam as hard as you guys one day!",
+                   "They won’t forget the day they lost to the Slambonis!",
+                   "You guys really left em skidding on the rink!", ],
+            "W2": ["Do you think we should go pro?", 
+                   "RLCS here we come!",
+                   "Maybe we should let them win for a change?",
+                   "Does this mean we get jerseys and would they make them in my size… and shape?"],
+            "W3": ["Guess what, I just signed us up for RLCS. I wasn’t joking earlier.",
+                   "If you guys keep this up I’ll have to get us a sponsor!",
+                   "Ok so I’m not pointing any wheels here but... You guys are reporting losing right?"],
+            "L1": ["Aw man…",
+                   "I still think we’re the best club around!",
+                   "At least we had fun",
+                   'Papa Boni always used to say "Sometimes you slam, sometimes you get slammed"',
+                   "Darn. I forgot what I was gonna to say…",
+                   "Well we’ve still got each other"],
+            "L2": ["Momma Boni said there'd be days like this.",
+                   "This would crush a weaker club. But not us!",
+                   "It’s not looking good. But. We’ve seen worse… Right?"],
+            "L3": ["This hurts. Does this hurt for you guys? This hurts me physically.",
+                   "Don’t worry guys. I have a feeling they’re getting tired of beating us!",
+                   "Hey guys. Can we take a water break? I need a minute."]}
 
         if match == "win":
             increment_win()
             stats = get_stats()
-            
+            if stats.win_streak <= 3: win_messages = Messages["W1"]
+            elif stats.win_streak <= 6: win_messages = Messages["W2"]
+            else: win_messages = Messages["W3"]
             await ctx.send(f"{random.choice(win_messages)} \nCurrent win streak is: {str(stats.win_streak)}")
 
         elif match == "loss":
             increment_loss()
             stats = get_stats()
-            
+            if stats.loss_streak <= 3: loss_messages = Messages["L1"]
+            elif stats.loss_streak <= 6: loss_messages = Messages["L2"]
+            else: loss_messages = Messages["L3"]
             await ctx.send(f"{random.choice(loss_messages)} \nCurrent loss streak is: {str(stats.loss_streak)}")
             
         elif match == "stats":
             stats = get_stats()
+            s_or_nah = "s"
             if stats.win_streak > stats.loss_streak:
-                streak_message = f"We have a current win streak of {stats.win_streak} games." 
+                if stats.win_streak == 1: s_or_nah = ""
+                streak_message = f"We have a current win streak of {stats.win_streak} game{s_or_nah}." 
             else: 
-                streak_message = f"We have a current loss streak of {stats.loss_streak} games." 
+                if stats.loss_streak == 1: s_or_nah = ""
+                streak_message = f"We have a current loss streak of {stats.loss_streak} game{s_or_nah}." 
+            
             embed=discord.Embed(title="⠀", color=0xf6c518)
             embed.set_author(name=f"Slambonis Season {stats.season}")
             embed.set_thumbnail(url="https://cdn.discordapp.com/icons/610818618325729281/a39b2a8d628ff0a1a20caf44c8e802e5.png")
