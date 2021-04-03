fa94b03311a9b41b49ff70465ec40bb0102d5a92
Thorulf Neustrup <thorulf@live.dk>
2021-04-03 19:13:43 +0200

    Simplified migrations

--- migrations/20210403_01_ReNbu-better-db.py
+++ migrations/20210403_01_ReNbu-better-db.py
@@ -9,7 +9,7 @@ __depends__ = {'20210325_01_Xdj8F-initial'}
 steps = [
     step("""
             CREATE TABLE matches (
-                id int(10) UNSIGNED NOT NULL,
+                id int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
                 win tinyint(1) NOT NULL,
                 overtime tinyint(1) DEFAULT NULL,
                 match_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -19,7 +19,7 @@ steps = [
 
     step("""
             CREATE TABLE seasons (
-                id int(10) UNSIGNED NOT NULL,
+                id int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
                 number int(10) UNSIGNED NOT NULL,
                 date_start timestamp NOT NULL,
                 date_end timestamp NOT NULL
@@ -30,40 +30,11 @@ steps = [
     step("""
             CREATE TABLE users_matches (
                 user_id bigint(20) UNSIGNED NOT NULL,
-                match_id int(10) UNSIGNED NOT NULL
+                match_id int(10) UNSIGNED NOT NULL,
+
+                PRIMARY KEY (user_id, match_id),
+                CONSTRAINT users_matches_FK FOREIGN KEY (match_id) REFERENCES matches (id)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
         """,
         "DROP TABLE users_matches;"),
-
-    step("""
-        ALTER TABLE matches
-        ADD PRIMARY KEY (id);
-        """),
-
-    step("""
-        ALTER TABLE seasons
-        ADD PRIMARY KEY (id);
-        """),
-
-    step("""
-        ALTER TABLE users_matches
-        ADD PRIMARY KEY (user_id,match_id),
-        ADD KEY users_matches_FK (match_id);
-        """),
-
-    step("""
-        ALTER TABLE matches
-        MODIFY id int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-        """),
-
-    step("""
-        ALTER TABLE seasons
-        MODIFY id int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-        """),
-
-    step("""
-        ALTER TABLE users_matches
-        ADD CONSTRAINT users_matches_FK FOREIGN KEY (match_id) REFERENCES matches (id);
-        """),
-
     ]
