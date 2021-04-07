b0988c70902fef33e212f27cc65ee5813fe4d32a
Wolfhound905 <aiden8green@gmail.com>
2021-04-07 17:14:33 -0400

    remove logging

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from configuration import get_token, get_guilds
-import logging
-
 
 intents = discord.Intents.default()
 intents.members = True
