6c2c42533685dac01123096127fe061c9594b039
Wolfhound905 <aiden8green@gmail.com>
2021-04-07 13:14:24 -0400

    fix bot.py, removed unused import from user_xp

--- bot.py
+++ bot.py
@@ -3,32 +3,33 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from configuration import get_token, get_guilds
+import logging
 
 
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="./", intents=intents)
-slash = SlashCommand(bot, sync_commands=True, override_type = True)
+slash = SlashCommand(bot, sync_commands=False, override_type = True)
 activity = discord.Activity(name='on the ice rink', type=discord.ActivityType.playing)
 
 
+bot.load_extension("behaviors.clubMatches")
+bot.load_extension("behaviors.createVC")
+bot.load_extension("behaviors.help")
+bot.load_extension("behaviors.copyCat")
+bot.load_extension("behaviors.adminCommands")
+bot.load_extension("behaviors.welcome")
+bot.load_extension("behaviors.xp")
+bot.load_extension("behaviors.slamberParty")
+bot.load_extension("behaviors.stats")
+bot.load_extension("behaviors.rewardRemover")
+
 @bot.event
 async def on_ready():
-    bot.load_extension("behaviors.clubMatches")
-    bot.load_extension("behaviors.createVC")
-    bot.load_extension("behaviors.help")
-    bot.load_extension("behaviors.copyCat")
-    bot.load_extension("behaviors.adminCommands")
-    bot.load_extension("behaviors.welcome")
-    bot.load_extension("behaviors.xp")
-    bot.load_extension("behaviors.slamberParty")
-    bot.load_extension("behaviors.stats")
-    bot.load_extension("behaviors.rewardRemover")
-
     print("ready")
     commands = slash.commands
-    print(" Commands ".center(14, "~"))
     await bot.change_presence(activity=activity)
+    print(" Commands ".center(14, "~"))
     for key in commands:
         print(commands[key].name)
     print("⸻⸻⸻⸻") 
