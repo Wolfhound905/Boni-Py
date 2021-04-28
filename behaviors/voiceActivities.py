d629a4d489a5a03158b5288fa742a9cca9f003f8
aiden <aiden8green@gmail.com>
2021-04-28 16:51:15 -0400

    custom activities for the future

--- behaviors/voiceActivities.py
+++ behaviors/voiceActivities.py
@@ -17,55 +17,77 @@ class voiceActivities(commands.Cog):
         {          
             "name": "activity_type",
             "description": "Type of activity.",
-            "required": True,
+            "required": False,
             "type": 3,
             "choices": [
-                { "name": "YouTube Together", "value": "1"}, { "name": "Betrayal.io", "value": "2" },
-                { "name": "Poker Night", "value": "3"}, { "name": "Fishington.io", "value": "4" }
+                { "name": "YouTube Together", "value": "755600276941176913"},
+                { "name": "Betrayal.io", "value": "773336526917861400"},
+                { "name": "Poker Night", "value": "755827207812677713"},
+                { "name": "Fishington.io", "value": "814288819477020702"}
             ]
+        },
+        {
+            "name": "custom_id",
+            "description": "Don't use this unless you know what you are doing 🙃",
+            "required": False,
+            "type": 3
         }
     ]
-
-    activities = {
-        "1": { "name": "YouTube Together", "id": "755600276941176913", "logo": "https://cdn.discordapp.com/attachments/780435741650059268/836350004204798010/yt-icon.png"},
-        "2": { "name": "Betrayal", "id": "773336526917861400", "logo": "https://betrayal.io/asset/image/share-card-betrayal.png"},
-        "3": { "name": "Poker Night", "id": "755827207812677713", "logo": "https://media.discordapp.net/attachments/780435741650059268/836354650013171772/unknown.png?width=822&height=676"},
-        "4": { "name": "Fishington", "id": "814288819477020702", "logo": "https://betrayal.io/asset/image/share-card-fishington.png"}
-    }
-
     async def get_activity(self, url, api_json, headers):
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=api_json, headers=headers) as response:
                 data = json.loads(await response.text())
-                code = data["code"]
-                return code
+                try:
+                    code = data["code"]
+                    name = data["target_application"]["name"]
+                    logo = f"https://cdn.discordapp.com/app-icons/{data['target_application']['id']}/{data['target_application']['icon']}"
+                    activity = {
+                        "code": code,
+                        "name": name,
+                        "logo": logo
+                    }
+                except KeyError:
+                    activity = {
+                        "error": f"{data}"
+                    }
+    
+                return activity
 
     @cog_ext.cog_slash(name="activities", options=options, description='Create a voice channel activity', guild_ids=guilds)
-    async def group_say(self, ctx: SlashContext, activity_type):
-        if ctx.author.voice:
-            url = f"https://discord.com/api/v9/channels/{ctx.author.voice.channel.id}/invites"
-            api_json = {
-                "max_age": 86400,
-                "max_uses": 0,
-                "target_application_id": f"{self.activities[activity_type]['id']}",
-                "target_type": 2,
-                "temporary": False,
-                "validate": None
-                }
-            headers = {
-                "Authorization": f"Bot {get_token()}",
-                "Content-Type": "application/json"
-                }
-
-            code = await self.get_activity(url, api_json, headers)
-
-
-            embed=discord.Embed(title=f"Join {ctx.author.name} in {self.activities[activity_type]['name']}", description=f"[Click this link](https://discord.gg/{code}) to start or join {self.activities[activity_type]['name']} in {ctx.author.voice.channel.name}")
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-            embed.set_thumbnail(url=self.activities[activity_type]['logo'])
-            await ctx.send(embed=embed)
+    async def group_say(self, ctx: SlashContext, activity_type = None, custom_id = None):
+        if activity_type is None and custom_id is None:
+            await ctx.send(hidden=True, content="Pick an activity.")
         else:
-            await ctx.send(hidden=True, content="You need to be in a voice channel.")
+            if ctx.author.voice:
+                url = f"https://discord.com/api/v9/channels/{ctx.author.voice.channel.id}/invites"
+                api_json = {
+                    "max_age": 86400,
+                    "max_uses": 0,
+                    "uses": 1,
+                    "target_application_id": f"{activity_type if custom_id is None else custom_id}",
+                    "target_type": 2,
+                    "temporary": False,
+                    "validate": None
+                    }
+                headers = {
+                    "Authorization": f"Bot {get_token()}",
+                    "Content-Type": "application/json"
+                    }
+
+
+                activity = await self.get_activity(url, api_json, headers)
+                try:
+                    x = activity["error"]
+                    await ctx.send(f"`{x}` Probably means application you are trying to play doesn't exist.🙃")
+                except KeyError:                        
+                    embed=discord.Embed(color=0x7289DA, title=f"Join {ctx.author.name} in {activity['name']}", description=f"[Click this link](https://discord.gg/{activity['code']}) to start or join {activity['name']} in {ctx.author.voice.channel.name}" )
+                    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+                    embed.set_thumbnail(url=activity['logo'])
+                    await ctx.send(embed=embed)
+
+            else:
+                await ctx.send(hidden=True, content="You need to be in a voice channel.")
+
         
 def setup(bot):
     bot.add_cog(voiceActivities(bot))
