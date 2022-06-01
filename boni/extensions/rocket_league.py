7778c66b379c7d4decde7982a03a307e2283e5f1
Aiden <aiden8green@gmail.com>
2022-05-31 23:43:31 -0400

    feat: rocket league tourneys

---
+++ boni/extensions/rocket_league.py
@@ -0,0 +1,95 @@
+import asyncio
+import naff
+from datetime import timedelta
+
+from boni.utils.rocket_league import Tournament, get_tourneys
+
+
+class RocketLeague(naff.Extension):
+    def __init__(self, bot: naff.Client):
+        self.bot: naff.Client = bot
+
+    rl_base_cmd = naff.SlashCommand(name="rocket_league", description="Rocket League commands")
+
+    @rl_base_cmd.subcommand(
+        sub_cmd_name="tourneys", sub_cmd_description="Get upcoming tourneys"
+    )
+    @naff.slash_option(
+        "region",
+        description="Effects what tourneys are show",
+        opt_type=naff.OptionTypes.STRING,
+        required=False,
+        choices=[
+            naff.SlashCommandChoice("US East", "USE"),
+            naff.SlashCommandChoice("US West", "USW"),
+            naff.SlashCommandChoice("Europe", "EU"),
+        ],
+    )
+    async def tourneys(self, ctx: naff.InteractionContext, region: str = "USE") -> None:
+        tourneys = await get_tourneys(region)
+        embed = naff.Embed(
+            title="Rocket League Tourneys",
+            color="#0099ff",
+            thumbnail="https://cdn.discordapp.com/attachments/819980437329543180/981374008031330354/unknown.png",
+        )
+
+        select_options = []
+        for tourney in tourneys[:3]:
+            time_str = tourney.timestamp.strftime("%I:%M %p")
+            select_options.append(
+                naff.SelectOption(
+                    tourney.name, str(tourney.timestamp.timestamp()), time_str
+                )
+            )
+            start_time = tourney.timestamp.format(naff.TimestampStyles.ShortTime)
+            relative_time = tourney.timestamp.format(naff.TimestampStyles.RelativeTime)
+            embed.add_field(
+                name=tourney.name,
+                value=f"Starts at {start_time} ({relative_time})",
+                inline=False,
+            )
+
+        select = naff.Select(select_options, placeholder="Sign me up for a tourney!")
+        sent = await ctx.send(embed=embed, components=select)
+
+        try:
+            select_resp = await self.bot.wait_for_component(
+                components=select,
+                timeout=60,
+                check=lambda x: x.context.author.id == ctx.author.id,
+            )
+            await self.handle_tourney_select(select_resp.context, tourneys)
+
+        except asyncio.TimeoutError:
+            disabled = naff.Select(
+                select_options, placeholder="Run the command again!", disabled=True
+            )
+            await sent.edit(components=disabled)
+
+    async def handle_tourney_select(
+        self, ctx: naff.ComponentContext, tourneys: list
+    ) -> None:
+        selected: "Tournament" = [
+            tourney
+            for tourney in tourneys
+            if str(tourney.timestamp.timestamp()) == ctx.values[0]
+        ][0]
+        event = await ctx.guild.create_scheduled_event(
+            f"Rocket League {selected.name} Tourney",
+            event_type=naff.ScheduledEventType.EXTERNAL,
+            start_time=selected.timestamp - timedelta(minutes=15),
+            description=f"{ctx.author.mention} wants to play a {selected.name} tourney! Sign up to get notified before it starts!",
+            external_location=f"{selected.name} at {selected.timestamp.strftime('%I:%M %p')}",
+            end_time=selected.timestamp + timedelta(minutes=15),
+        )
+        event_invite = f"https://discord.com/events/{ctx.guild.id}/{event.id}"
+
+        await ctx.edit_origin(
+            f"<@&698631135919341599> | {ctx.author.mention} wants to play in a tourney.\n Here is [event]({event_invite}) to remind you. Make sure to click **Interested**!",
+            components=[],
+            embed=[],
+        )
+
+
+def setup(bot: naff.Client):
+    RocketLeague(bot)
---
+++ boni/utils/rocket_league.py
@@ -0,0 +1,79 @@
+from typing import List, Optional
+import aiohttp
+from attr import define
+from bs4 import BeautifulSoup
+from datetime import datetime
+from dateutil import tz
+import naff
+from naff.client.mixins.serialization import DictSerializationMixin
+from naff.client.utils.attr_utils import field
+from naff.client.utils.attr_converters import optional as c_optional
+from naff.client.utils.attr_converters import timestamp_converter
+
+
+UTC = tz.gettz("UTC")
+
+
+async def get_tourneys(region: str = "USE") -> Optional[List["Tournament"]]:
+    """
+    Grabs upcoming tourneys for specified region
+
+    Regions:
+        USE: US East
+        USW: US West
+        EU: Europe
+    """
+    torneys = []
+    jar = aiohttp.CookieJar(unsafe=True)
+    async with aiohttp.ClientSession(cookie_jar=jar) as session:
+        async with session.get("https://rocket-league.com/tournaments") as resp:
+            page_soup = BeautifulSoup(await resp.text(), "html.parser")
+            raw_csrf = page_soup.find(id="window-csrf").text
+            csrf_token = raw_csrf.split("=")[1].strip().strip('"')
+
+        data = f"csrf_token={csrf_token}&region={region}"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+        # This first request is to get the CSRF token and needed cookies
+
+        async with session.post(
+            "https://rocket-league.com/functions/updateTournamentRegionPref.php",
+            data=data,
+            headers=headers,
+        ) as resp:
+            page_soup = BeautifulSoup(await resp.text(), "html.parser")
+            tourneys_html = page_soup.find_all(attrs={"class": "rlg-tournament__item"})
+
+            for tourney_html in tourneys_html:
+                tourney_soup = BeautifulSoup(str(tourney_html), "html.parser")
+                if datetime_str := tourney_soup.find(
+                    attrs={"class": "rlg-tournament__item-timer"}
+                ):
+                    time_raw = datetime.strptime(
+                        datetime_str.get("data-time"), "%Y-%m-%d %H:%M:%S"
+                    )
+                    zone_aware_time = time_raw.replace(tzinfo=UTC)
+                    time = zone_aware_time.timestamp()
+                else:
+                    continue
+
+                img = tourney_soup.find(attrs={"class": "rlg-tournament__item-img"})[
+                    "src"
+                ]
+                name = (
+                    tourney_soup.find(attrs={"class": "rlg-tournament__item-info"})
+                    .find("h2")
+                    .text
+                )
+                tourney = {"timestamp": time, "img": img, "name": name}
+                torneys.append(Tournament.from_dict(tourney))
+    return torneys
+
+
+@define()
+class Tournament(DictSerializationMixin):
+    name: str = field(default="N/A", repr=True)
+    img: str = field(default="N/A", repr=True)
+    timestamp: Optional["naff.Timestamp"] = field(
+        default="In progress...", repr=True, converter=c_optional(timestamp_converter)
+    )
