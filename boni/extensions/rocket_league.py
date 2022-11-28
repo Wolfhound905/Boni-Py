b38b53180bc6753955cea644c49e15f752478d4a
Wolfhound905 <aiden8green@gmail.com>
2022-11-28 02:47:49 +0000

    and the imports

--- boni/extensions/rocket_league.py
+++ boni/extensions/rocket_league.py
@@ -1,9 +1,4 @@
-import asyncio
 import naff
-from datetime import timedelta
-
-from boni.utils.rocket_league import Tournament, get_tourneys
-
 
 class RocketLeague(naff.Extension):
     def __init__(self, bot: naff.Client):
